9fcb92df253a18079577fc0066263fcc432b08a3
mihaivaidos <mihai@vaidos.com>
2025-02-18 12:05:39 +0200

    Created the last method that writes in file (d).

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,7 +21,9 @@ public class Main {
 
         //filterNinjasByPoints(ninjas);
 
-        filterStufeJonin(ninjas);
+        //filterStufeJonin(ninjas);
+
+        writeNumberNinjaProStufe(ninjas);
 
     }
 
@@ -75,6 +77,52 @@ public class Main {
         System.out.println("All ninjas with Stufe Jonin: ");
         ninjas.stream().filter(n -> n.getStufe().equals("Jonin")).sorted(Comparator.comparing(Ninja::getDatum).reversed())
                 .map(n -> n.getDatum() + ": " + n.getCharacterName() + " - " + n.getBeschreibung()).forEach(System.out::println);
+    }
+
+    public static void writeNumberNinjaProStufe(List<Ninja> ninjas) {
+
+        Map<String, Integer> numberNinjas = new HashMap<>();
+        Map<String, Double> totalPointsStufe = new HashMap<>();
+        for (Ninja n : ninjas) {
+            String stufe = n.getStufe();
+            numberNinjas.put(stufe, numberNinjas.getOrDefault(stufe, 0) + 1);
+            totalPointsStufe.put(stufe, totalPointsStufe.getOrDefault(stufe, 0.0) + n.getKraftpunkte());
+        }
+
+        List<Map.Entry<String, Integer>> sortedNinjas = numberNinjas.entrySet().stream().sorted((c1, c2) -> {
+            int countCompare = Integer.compare(c2.getValue(), c1.getValue());
+            if (countCompare == 0) {
+                return c1.getKey().compareTo(c2.getKey());
+            }
+            return countCompare;
+        }).toList();
+
+        List<Map.Entry<String, Double>> sortedPoints = totalPointsStufe.entrySet().stream().sorted(Comparator.comparingDouble(Map.Entry::getValue)).toList();
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("src/gesammtzahl.txt"))) {
+            for (Map.Entry<String, Integer> entry : sortedNinjas) {
+                if(entry.getKey().equals("Genin")) {
+                    bw.write(entry.getKey() + "%" + entry.getValue() + "#" + sortedPoints.stream().filter(c -> c.getKey().equals("Genin")).findFirst().get().getValue());
+                    bw.newLine();
+                }
+                if(entry.getKey().equals("Kage")) {
+                    bw.write(entry.getKey() + "%" + entry.getValue() + "#" + sortedPoints.stream().filter(c -> c.getKey().equals("Genin")).findFirst().get().getValue());
+                    bw.newLine();
+                }
+                if(entry.getKey().equals("Chunin")) {
+                    bw.write(entry.getKey() + "%" + entry.getValue() + "#" + sortedPoints.stream().filter(c -> c.getKey().equals("Chunin")).findFirst().get().getValue());
+                    bw.newLine();
+                }
+                if(entry.getKey().equals("Jonin")) {
+                    bw.write(entry.getKey() + "%" + entry.getValue() + "#" + sortedPoints.stream().filter(c -> c.getKey().equals("Jonin")).findFirst().get().getValue());
+                    bw.newLine();
+                }
+            }
+            System.out.println("\nNumber ninjas saved to 'gesammtzahl.txt'.");
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+
     }
 
 }
